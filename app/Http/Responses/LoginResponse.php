e7b8b839245231b11449d8c96864a46d9f32e427
Divine Idehen <idehendivine16@gmail.com>
2025-12-27 18:00:19 +0100

    feat: Integrate CartService into LoginResponse and RegisterResponse for cart migration on user login and registration

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -3,15 +3,22 @@
 namespace App\Http\Responses;
 
 use App\Enum\UserRoleEnum;
+use App\Services\Cart\CartService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 
 class LoginResponse implements LoginResponseContract
 {
+    public function __construct(
+        protected CartService $cartService
+    ) {}
+
     public function toResponse($request): RedirectResponse|JsonResponse
     {
         $user = auth()->user();
+
+        $this->cartService->migrateCart($request);
 
         if ($user->hasAnyRole([UserRoleEnum::SUPER_ADMIN->name, UserRoleEnum::ADMIN->name])) {
             return redirect()->intended('/dashboard');
--- app/Http/Responses/RegisterResponse.php
+++ app/Http/Responses/RegisterResponse.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Responses;
 
+use App\Services\Cart\CartService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
 
 class RegisterResponse implements RegisterResponseContract
 {
+    public function __construct(
+        protected CartService $cartService
+    ) {}
+
     public function toResponse($request): RedirectResponse|JsonResponse
     {
+        $user = auth()->user();
+
+        $this->cartService->migrateCart($request);
+
         return redirect('/');
     }
 }
