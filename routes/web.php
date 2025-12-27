7d307eb0a446a088f8ca1b31ea0279455cc3fe96
Divine Idehen <idehendivine16@gmail.com>
2025-12-27 18:01:57 +0100

    feat: Define routes for CartController to manage cart operations including viewing, adding, updating, removing, and clearing cart items

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -7,6 +8,12 @@ use Inertia\Inertia;
 Route::get('/', [ProductController::class, 'index'])->name('home');
 
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
+Route::put('/cart/{cartItemId}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/{cartItemId}', [CartController::class, 'destroy'])->name('cart.destroy');
+Route::delete('/cart', [CartController::class, 'clear'])->name('cart.clear');
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('dashboard', function () {
